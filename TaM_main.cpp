e5aa8d78c89f0770e1e74da5ecfddd4da44f34d9
James Vaughan <James.Earl.Vaughan@gmail.com>
2014-01-08 21:25:09 -0500

    OpenGL Environment Established
    The window for OpenGL opens and I can close it with ESC. Major
    progress!!!!

--- TaM_main.cpp
+++ TaM_main.cpp
@@ -1,8 +1,70 @@
 // The official start for Theseus and Minnie. Initializes app and yield control to TaM_Master.
 
 #include "TaM_defHeaders.h"
+#include "TaM_main.h"
+
+using namespace std;
+
+#define	TaM_WINDOW_X	640
+#define TaM_WINDOW_Y	480
+
 
 int main(int argc, char *args[]) {
+	// The following section will be pruned and rewritten as major features develop
+	// Initialize library
+	if (!glfwInit()) {
+		// Well that sucks, bye
+		return 0;
+	}
 
+	GLFWwindow *theWnd = TaM_viewInit_direct(); 
+	if (!theWnd) {
+		// Just quietly crash, we can debug for faults
+		return 0;
+	}
+
+	
+	TaM_mainloop_direct(theWnd);
+	
+
+	// Again, the following section will be pruned of the _direct functions
+	TaM_viewDestroy_direct(theWnd);
+
+	glfwTerminate();
 	return 0;
+}
+
+GLFWwindow *TaM_viewInit_direct() {
+	GLFWwindow *ptrW = glfwCreateWindow(TaM_WINDOW_X, TaM_WINDOW_Y, "Theseus and the Minotaur - Tempy", NULL, NULL);
+	if (!ptrW) {
+		return NULL;
+	}
+
+	glfwSetKeyCallback(ptrW, TaM_kbCallback_direct);
+
+	glfwMakeContextCurrent(ptrW);
+
+	// Everything go well? Groovy!
+	return ptrW;
+}
+
+void TaM_viewDestroy_direct(GLFWwindow *ptrW) {
+	glfwDestroyWindow(ptrW);
+}
+
+static void TaM_kbCallback_direct(GLFWwindow *ptrW, int key, int sCode, int act, int mods) {
+	if (act == GLFW_PRESS) {
+		switch (key) {
+		case GLFW_KEY_ESCAPE:
+			glfwSetWindowShouldClose(ptrW, GL_TRUE);
+			break;
+		}
+	}
+}
+
+void TaM_mainloop_direct(GLFWwindow *ptrW) {
+	while(!glfwWindowShouldClose(ptrW)) {
+
+		glfwPollEvents();
+	}
 }
--- TaM_main.h
+++ TaM_main.h
@@ -1,1 +1,54 @@
 // Header for the start of Theseus and Minne. May or may not ever be needed...
+
+#include "TaM_defHeaders.h"
+
+/***** TaM_viewInit_direct
+	* Initializes the view window for the entire app.
+	* For initial baby builds only.
+	* Replace with: to be determined
+
+	*** Parameters:
+
+	*** Returns:
+	A pointer to the application window or NULL if creation failed
+
+*/
+GLFWwindow *TaM_viewInit_direct();
+
+/***** TaM_viewDestroy_direct
+	* Cleans up after GLFWwindow to close app.
+	* For initial baby builds only.
+	* Replace with: to be determined
+
+	*** Parameters:
+	* ptrW	- pointer to a GLFWwindow	
+
+*/
+void TaM_viewDestroy_direct(GLFWwindow *ptrW);
+
+/***** TaM_kbCallback_direct()
+	* Keyboard callback
+	* Replace with: to be determined
+
+	*** Parameters:
+	* ptrW	- pointer to GLFWwindow to be linked with
+	* key	- key pressed
+	* sCode	- scancode
+	* act	- action
+	* mods	- mods, dunno
+
+	*** Returns:
+	
+*/
+static void TaM_kbCallback_direct(GLFWwindow *ptrW, int key, int sCode, int act, int mods);
+
+/***** TaM_mainloop_direct()
+	* The mainloop for TaM
+	* Replace with: to be determined
+
+	*** Parameters:
+
+	*** Returns:
+	
+*/
+void TaM_mainloop_direct(GLFWwindow *ptrW);
